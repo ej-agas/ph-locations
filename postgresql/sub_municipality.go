541502f7bf975ece8927d91d91613fee2b4e0719
ej-agas <agas.ej09@gmail.com>
2023-07-21 23:23:00 +0800

    add sub_municipality store
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

--- postgresql/sub_municipality.go
+++ postgresql/sub_municipality.go
@@ -20,4 +20,68 @@ func (store SubMunicipalityStore) Save(ctx context.Context, subMunicipality mode
 	if err != nil {
 		return fmt.Errorf("error creating prepared statement: %w", err)
 	}
+
+	defer stmt.Close()
+
+	if _, err := stmt.Exec(
+		subMunicipality.Code,
+		subMunicipality.Name,
+		subMunicipality.Population,
+		subMunicipality.CityId,
+	); err != nil {
+		return fmt.Errorf("error executing query: %w", err)
+	}
+
+	return nil
+}
+
+func (store SubMunicipalityStore) Find(id int) (models.SubMunicipality, error) {
+	var subMunicipality models.SubMunicipality
+
+	row := store.db.QueryRow("SELECT * FROM sub_municipalities WHERE id = $1", id)
+	err := row.Scan(&subMunicipality.Id, &subMunicipality.Code, &subMunicipality.Name, &subMunicipality.Population)
+
+	if err == nil {
+		return subMunicipality, nil
+	}
+
+	if err == sql.ErrNoRows {
+		return subMunicipality, fmt.Errorf("SubMunicipality with id = %d not found: %w", id, err)
+	}
+
+	return subMunicipality, fmt.Errorf("error executing query: %w", err)
+}
+
+func (store SubMunicipalityStore) FindByCode(code string) (models.SubMunicipality, error) {
+	var subMunicipality models.SubMunicipality
+
+	row := store.db.QueryRow("SELECT * FROM sub_municipalities WHERE code = $1", code)
+	err := row.Scan(&subMunicipality.Id, &subMunicipality.Code, &subMunicipality.Name, &subMunicipality.Population)
+
+	if err == nil {
+		return subMunicipality, nil
+	}
+
+	if err == sql.ErrNoRows {
+		return subMunicipality, fmt.Errorf("SubMunicipality with code = %s not found: %w", code, err)
+	}
+
+	return subMunicipality, fmt.Errorf("error executing query: %w", err)
+}
+
+func (store SubMunicipalityStore) FindByName(name string) (models.SubMunicipality, error) {
+	var subMunicipality models.SubMunicipality
+
+	row := store.db.QueryRow("SELECT * FROM sub_municipalities WHERE name = $1", name)
+	err := row.Scan(&subMunicipality.Id, &subMunicipality.Code, &subMunicipality.Name, &subMunicipality.Population)
+
+	if err == nil {
+		return subMunicipality, nil
+	}
+
+	if err == sql.ErrNoRows {
+		return subMunicipality, fmt.Errorf("SubMunicipality with name = %s not found: %w", name, err)
+	}
+
+	return subMunicipality, fmt.Errorf("error executing query: %w", err)
 }
